346996e9a3638162a4eec363ebc7ec16719a7b16
Ivanna Navrotska <navrockaivanna15@gmail.com>
2024-04-16 09:22:27 +0300

    Create code_unittests.py

---
+++ code_unittests.py
@@ -0,0 +1,27 @@
+import unittest
+import code
+
+class TestCode(unittest.TestCase):
+
+
+    def test_fibonacci(self):
+
+        result = code.fibonacci(0)
+        self.assertEqual(result, 1)
+
+        result = code.fibonacci(1)
+        self.assertEqual(result, 1)
+
+        result = code.fibonacci(6)
+        self.assertEqual(result, 13)
+
+#it's time for negative testing
+
+        result = code.fibonacci(5)
+        self.assertNotEqual(result, 3)
+        
+        self.assertFalse(code.fibonacci('five'))  
+        self.assertFalse(code.fibonacci(-5))
+
+if __name__ == '__main__':
+    unittest.main()
